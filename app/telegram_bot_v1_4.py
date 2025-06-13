906e3ce532b64f1053f5360f783302b77a5f57f8
Sanjay <sanjay@DELL-PC.sanjay-lab.local>
2025-06-13 17:04:39 +0530

    New Updates

--- app/telegram_bot_v1_4.py
+++ app/telegram_bot_v1_4.py
@@ -11,7 +11,7 @@ import re
 
 # Define categories
 INCOME_CATEGORIES = ['Salary', 'Investment', 'Bonus', 'Other']
-EXPENSE_CATEGORIES = ['Food', 'Utilities', 'Shopping', 'Medical', 'Travel', 'Groceries', 'Entertainment', 'Investments', 'Other']
+EXPENSE_CATEGORIES = ['Food', 'Utilities', 'Shopping', 'Medical', 'Travel', 'Groceries', 'Entertainment', 'Investments', 'Loans', 'Other']
 
 # Define stages
 #START, AMOUNT, TYPE, CATEGORY, DESCRIPTION = range(5)
